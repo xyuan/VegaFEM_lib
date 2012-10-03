df51513a440bbb9a40fdb6af811f11f0301d985d
Stephen <stephen.liziyun@gmail.com>
2012-10-03 17:09:00 -0400

    delete glui and some unused source files

--- include/lapack-headers.h
+++
@@ -1,9 +0,0 @@
-#if defined(WIN32) || defined(linux)
-  #include "mkl_cblas.h"
-  #include "mkl_types.h"
-  #include "mkl_lapack.h"
-  #include "mkl_blas.h"
-#elif defined(__APPLE__)
-  #include <vecLib/vecLib.h>
-#endif
-
